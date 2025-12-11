3afb0c56f70e924b9d1b119e5ea5c80aa21a3616
Vinay Parit <vinaypparit@gmail.com>
2025-12-11 19:13:56 +0530

    updated!

---
+++ Animated Components/Parallax Scrolling/script.js
@@ -0,0 +1,51 @@
+smoothScroll();
+
+
+
+
+
+
+
+
+
+
+
+const sections = [...document.querySelectorAll(".parallax-section")];
+
+sections.forEach(section =>{
+    const img = section.querySelector("img");
+
+    gsap.fromTO(
+      img,
+      {
+        yPercent: -30,
+      },
+      {
+        yPercent: 30,
+        ease: "none",
+        scrollTrigger:{
+            Trigger: section,
+            markers: true, 
+            scrub: true,
+        }
+      }
+    );
+})
+
+
+
+
+
+
+
+function smoothScroll() {
+    const lenis = new Lenis();
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+    gsap.ticker.lagSmoothing(0);
+
+}
